0214e0483438f0dc9b836a63b652f429f45d145e
Nagy Gergő <nagy.gergi6@gmail.com>
2021-10-27 08:54:35 +0200

    fix(client): Update dispatch type error

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -55,7 +55,7 @@ export const likePost = async (req, res) => {
 
     const post = await PostMessage.findById(id)
     const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true } )
-    console.log(updatedPost)
+    
     res.json(updatedPost)
 
 }
